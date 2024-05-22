16da2a5eacc78fcee7faed64833b42fad0adea96
Alex_DGL112 <legendgg211@gmail.com>
2024-05-22 09:42:36 +0700

    new

---
+++ to-do-list/register.js
@@ -0,0 +1,17 @@
+document.getElementById("registerForm").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const username = document.getElementById("username")
+    const password = document.getElementById("password")
+
+    const registerButton = document.getElementById("Register-button")
+
+    registerButton.onclick = () => {
+        localStorage.setItem("username", username.value)
+        localStorage.setItem("password", password.value)
+
+        alert("User Succesfully Registered")
+        window.location.href = "home.html"
+
+        console.log("Users Registered")
+    }
+})
